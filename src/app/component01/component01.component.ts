623e03943d38818ebc70397409e9855b408f5293
dhanushkak <dhanushkak@scienter.lk>
2023-03-26 02:21:35 +0530

    new version with component view change

--- src/app/component01/component01.component.ts
+++ src/app/component01/component01.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Component01Component implements OnInit {
 
+
+  dummyValues = [
+    ['Value 1', 'Value 2', 'Value 3', 'Value 4', 'Value 5', 'Value 6'],
+    ['Value 7', 'Value 8', 'Value 9', 'Value 10', 'Value 11', 'Value 12'],
+    ['Value 13', 'Value 14', 'Value 15', 'Value 16', 'Value 17', 'Value 18'],
+    ['Value 19', 'Value 20', 'Value 21', 'Value 22', 'Value 23', 'Value 24']
+  ];
+  
   constructor() { }
 
   ngOnInit(): void {
