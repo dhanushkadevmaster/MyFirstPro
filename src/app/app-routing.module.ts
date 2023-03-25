6f7c900f465358bd2bfc63f338fd31fe2be3027e
dhanushkak <dhanushkak@scienter.lk>
2023-03-26 01:32:20 +0530

    added component01 and component02

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Component01Component } from './component01/component01.component';
+import { Component02Component } from './component02/component02.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: '/component1', pathMatch: 'full' },
+  { path: 'component01', component: Component01Component },
+  { path: 'component02', component: Component02Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
